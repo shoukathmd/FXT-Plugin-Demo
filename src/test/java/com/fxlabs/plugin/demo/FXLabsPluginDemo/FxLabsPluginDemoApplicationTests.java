312f8e37cc9a4588e7a39dc07cd0dca4bb26613a
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2018-09-21 17:17:59 +0530

    First commit

---
+++ src/test/java/com/fxlabs/plugin/demo/FXLabsPluginDemo/FxLabsPluginDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fxlabs.plugin.demo.FXLabsPluginDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FxLabsPluginDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
